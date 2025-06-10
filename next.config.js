f6172a038a805e1afe12d272dfe39d4b6588cf17
johny chnouda <johnychnouda@gmail.com>
2025-06-11 02:37:41 +0300

    fix: remove invalid experimental option

--- next.config.js
+++ next.config.js
@@ -12,10 +12,6 @@ const nextConfig = {
     domains: ['127.0.0.1', 'varda.hellotree.dev','admin.varda.ag', 'picsum.photos', 'store.storeimages.cdn-apple.com', 'images.unsplash.com'],
   },
 
-  experimental: {
-    disableVercelToolbar: true,
-  },
-
   env: {
     NEXT_DISABLE_VERCEL_TOOLBAR: '1',
   },
