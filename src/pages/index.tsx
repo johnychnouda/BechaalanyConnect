a67e1dcb1c3bf623b2bb18fb849a4d917976eecd
carloschnouda96 <carlos.chnouda@outlook.com>
2025-07-04 12:59:36 +0300

    Refactor HomeContent component: Add conditional rendering for categories, featured products, and latest products sections to improve layout and performance.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 function HomeContent() {
   const { homepageData } = useHomepageContext();
   const bannerSwiper = homepageData?.bannerSwiper || [];
-  const homepageSettings = homepageData?.homepageSettings ;
+  const homepageSettings = homepageData?.homepageSettings;
   const categories = homepageData?.categories || [];
   const featuredProducts = homepageSettings?.featured_products || [];
   const latestProducts = homepageData?.latest_products || [];
@@ -32,22 +32,31 @@ function HomeContent() {
     <PageLayout className={`flex flex-col min-h-screen gap-16 pb-32`}>
       <HomePageHeader bannerSwiper={bannerSwiper} homepageSettings={homepageSettings} />
       <section className="flex flex-col gap-20 px-6 md:px-12">
-        <PageGrid
-          items={categories}
-          label={homepageSettings?.categories_section_title}
-          renderItem={(item) => <Category key={item.id} category={item} />}
-          viewMoreHref="/categories"
-        />
-        <PageGrid
-          items={featuredProducts}
-          label={homepageData.homepageSettings.featured_products_section_title}
-          renderItem={(item) => <Product key={item.id} product={item} />}
-        />
-        <PageGrid
-          items={latestProducts}
-          label={homepageData.homepageSettings.latest_products_section_title}
-          renderItem={(item) => <Product key={item.id} product={item} />}
-        />
+        {/* Categories */}
+        {categories.length > 0 && (
+          <PageGrid
+            items={categories}
+            label={homepageSettings?.categories_section_title}
+            renderItem={(item) => <Category key={item.id} category={item} />}
+            viewMoreHref="/categories"
+          />
+        )}
+        {/* Featured Products */}
+        {featuredProducts.length > 0 && (
+          <PageGrid
+            items={featuredProducts}
+            label={homepageSettings?.featured_products_section_title}
+            renderItem={(item) => <Product key={item.id} product={item} />}
+          />
+        )}
+        {/* Latest Products */}
+        {latestProducts.length > 0 && (
+          <PageGrid
+            items={latestProducts}
+            label={homepageSettings?.latest_products_section_title}
+            renderItem={(item) => <Product key={item.id} product={item} />}
+          />
+        )}
       </section>
     </PageLayout>
   );
